cad82db506250f6bb5d51484ef77107833b5c1ed
Jared Chapman <thejaredchapman@gmail.com>
2023-06-15 22:07:41 -0500

    add static file

--- notes/views.py
+++ notes/views.py
@@ -1,13 +1,17 @@
 from typing import List
 from django.shortcuts import render
 from django.http import Http404
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 
 from .models import Notes
 
 class NotesListView(ListView):
     model = Notes
     context_object_name = "notes"
+
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = "note"
 
 def detail(request, pk):
     try:
