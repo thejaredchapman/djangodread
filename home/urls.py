c885e88e6b638995c44366d977f203e10f5cad4c
Jared Chapman <thejaredchapman@gmail.com>
2023-06-15 22:51:55 -0500

    uniform css

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.HomeView.as_view()),
-    path('authorized', views.AuthorizedView.as_view()),
+    path('home', views.HomeView.as_view),
+    path('authorized', views.AuthorizedView.as_view),
 ]
