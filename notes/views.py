80fedf7c37fcceb82a75e29b3aec78c15645e351
Jared Chapman <thejaredchapman@gmail.com>
2023-06-16 08:19:31 -0500

    add html files

--- notes/views.py
+++ notes/views.py
@@ -1,14 +1,27 @@
 from typing import List
 from django.shortcuts import render
 from django.http import Http404
-from django.views.generic import CreateView, DetailView, ListView
+from django.views.generic import CreateView, DetailView, ListView, UpdateView
+from django.views.generic.edit import DeleteView
 
+from .forms import NotesForm
 from .models import Notes
+
+class NotesDeleteView(DeleteView):
+    model = Notes
+    success_url = '/smart/notes'
+    
+
+
+class NotesUpdateView(UpdateView):
+    model = Notes
+    fields = ['title', 'text']
+    form_class = NotesForm
 
 class NotesCreateView(CreateView):
     model = Notes
     fields = ['title', 'text']
-    success_url = '/smart/notes'
+    form_class = NotesForm
 
 class NotesListView(ListView):
     model = Notes
