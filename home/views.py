5e51df4bf560a02766ae1b7f5e889352c8c94c62
Jared Chapman <thejaredchapman@gmail.com>
2023-06-16 08:57:00 -0500

    update user privleges

--- home/views.py
+++ home/views.py
@@ -8,10 +8,17 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import UserCreationForm
 
+from django.shortcuts import redirect
+
 class SignUpView(CreateView):
     form_class = UserCreationForm
     template_name = "home/register.html"
     success_url = '/smart/notes'
+
+    def get(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('notes.list')
+        return super().get(request, *args, **kwargs)
 
 class LoginInterfaceView(LoginView):
     template_name = 'home/login.html'
