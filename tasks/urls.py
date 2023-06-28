65afb96f3d41b135ff03f1fb23573c442fd7345f
Jared Chapman <thejaredchapman@gmail.com>
2023-06-28 14:14:04 -0500

    add button for welcome.html and fix urls for apps

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from . import views
+from tasks import views
 
 urlpatterns = [
-    path("", views.ListListView.as_view(), name="index"),
-    path("list/<int:list_id>/", views.ItemListView.as_view(), name="list"),
+    path('', views.ListListView.as_view(), name="tasks.index"),
+    path('list/<int:list_id>', views.ItemListView.as_view(), name="tasks.list"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView
-from .models import ToDoList, ToDoItem
+from tasks.models import ToDoList, ToDoItem
 
 class ListListView(ListView):
     model = ToDoList
