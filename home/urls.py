7506e50a212165b67b150e8a237f2f3aef68efdc
Jared Chapman <thejaredchapman@gmail.com>
2023-06-28 12:00:27 -0500

    update tasks

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('authorized', views.AuthorizedView.as_view(), name='authorized'),
