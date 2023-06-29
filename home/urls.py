17b3505a86d4a69413f29fb08021753bbc3c636d
Jared Chapman <thejaredchapman@gmail.com>
2023-06-29 12:01:50 -0500

    add user

--- home/urls.py
+++ home/urls.py
@@ -5,9 +5,9 @@ from . import views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
-    path('authorized/', views.AuthorizedView.as_view(), name='authorized'),
-    path('signup/', views.LoginInterfaceView.as_view(), name='login'),
-    path('logout/', views.LogoutInterfaceView.as_view(), name='logout'),
-    path('signup/', views.SignUpView.as_view(), name='signup'),
+    path('authorized', views.AuthorizedView.as_view(), name='authorized'),
+    path('signup', views.LoginInterfaceView.as_view(), name='login'),
+    path('logout', views.LogoutInterfaceView.as_view(), name='logout'),
+    path('signup', views.SignUpView.as_view(), name='signup'),
    
 ]
